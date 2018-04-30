8239b328286fcf5ffdd784f774766270a18ecdd8
Sean Huang <hyprul@ucla.edu>
2018-04-30 01:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  var newObj = Object.assign({}, object)
+  newObj.delete
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-
+  
 }
